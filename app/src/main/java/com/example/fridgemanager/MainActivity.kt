bb60b02946097626973f46e7116f3b5f840d9a27
stevey-sy <sinsy89@hanmail.net>
2021-03-28 01:45:09 +0900

    [Edit] MainActivity.kt / view binding 추가

--- app/src/main/java/com/example/fridgemanager/MainActivity.kt
+++ app/src/main/java/com/example/fridgemanager/MainActivity.kt
@@ -5,19 +5,25 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.fridgemanager.databinding.ActivityLoginBinding
+import com.example.fridgemanager.databinding.ActivityMainBinding
 import com.google.zxing.integration.android.IntentIntegrator
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        val button : Button = findViewById(R.id.button)
-        button.setOnClickListener {
+        // view binding 처리
+        binding.button.setOnClickListener {
             val integrator = IntentIntegrator(this)
             integrator.initiateScan();
-
         }
     }
 
