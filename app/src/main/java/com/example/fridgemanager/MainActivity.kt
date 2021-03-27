bc6d2ec36bda06b9c2d77afb35622f491137d4e6
stevey-sy <sinsy89@hanmail.net>
2021-03-28 01:37:41 +0900

    [Edit] 바코드 라이브러리 추가

--- app/src/main/java/com/example/fridgemanager/MainActivity.kt
+++ app/src/main/java/com/example/fridgemanager/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.example.fridgemanager
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.zxing.integration.android.IntentIntegrator
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button : Button = findViewById(R.id.button)
+        button.setOnClickListener {
+            val integrator = IntentIntegrator(this)
+            integrator.initiateScan();
+
+        }
+    }
+
+    // Get the results:
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if (result != null) {
+            if (result.contents == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
+            } else {
+                // 바코드 표시
+                Toast.makeText(this, "Scanned: " + result.contents, Toast.LENGTH_LONG).show()
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
     }
 }
