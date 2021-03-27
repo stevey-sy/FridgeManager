f0d0cbdc24931f5eef0e44133019bb39da547128
stevey-sy <sinsy89@hanmail.net>
2021-03-27 19:21:06 +0900

    [Edit] LoginActivity.kt / 구글계정으로 로그인하기 추가

--- app/src/main/java/com/example/fridgemanager/LoginActivity.kt
+++ app/src/main/java/com/example/fridgemanager/LoginActivity.kt
@@ -5,14 +5,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.example.fridgemanager.databinding.ActivityLoginBinding
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
 
     var auth : FirebaseAuth? = null
+    var GOOGLE_LOGIN_CODE = 9001
+    var googleSignInClient : GoogleSignInClient? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
@@ -23,6 +31,22 @@ class LoginActivity : AppCompatActivity() {
         binding.emailLoginButton.setOnClickListener {
             signinAndSignup()
         }
+        binding.googleSignInButton.setOnClickListener {
+            // 로그인 첫번째 단계
+            googleLogin()
+        }
+
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+    }
+
+    // google 로 로그인 버튼 눌렸을 때 사용할 메소드
+    fun googleLogin() {
+        var signInIntent = googleSignInClient?.signInIntent
+        startActivityForResult(signInIntent, GOOGLE_LOGIN_CODE)
     }
     // 로그인, 회원가입을 처리하는 메소드
     fun signinAndSignup() {
@@ -59,6 +83,33 @@ class LoginActivity : AppCompatActivity() {
     fun moveMainPage(user : FirebaseUser?) {
         if(user !=null) {
             startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == GOOGLE_LOGIN_CODE) {
+            var result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+            if(result!!.isSuccess) {
+                var account = result.signInAccount
+                // 로그인 두번째 단계
+                firebaseAuthWithGoogle(account)
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(account : GoogleSignInAccount?) {
+        var credential = GoogleAuthProvider.getCredential(account?.idToken, null)
+        auth?.signInWithCredential(credential)?.addOnCompleteListener {
+                task ->
+            if (task.isSuccessful) {
+                // id 와 password 가 일치했을 때 로그인 완료.
+                // 메인페이지로 이동
+                moveMainPage(task.result?.user)
+            } else {
+                // 일치하지 않았을 때
+                Toast.makeText(this, task.exception?.message, Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
