dd4aad3cb36453a982cde4a755a8361615a161ca
stevey-sy <sinsy89@hanmail.net>
2021-04-03 01:54:58 +0900

    [Edit] FridgeFragment.kt / toolbar + menu 추가

--- app/src/main/java/com/example/fridgemanager/FridgeFragment.kt
+++ app/src/main/java/com/example/fridgemanager/FridgeFragment.kt
@@ -1,32 +1,15 @@
 package com.example.fridgemanager
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [FridgeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class FridgeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        setHasOptionsMenu(true)
+
     }
 
     override fun onCreateView(
@@ -37,23 +20,16 @@ class FridgeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_fridge, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FridgeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FridgeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.main_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.add_fridge_item -> {
+                true
             }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
